079a69bfb09ba22fc2da93c9d7cce4394bdea94f
Mariane Candian <mariane.candian@gmail.com>
2022-07-19 16:50:00 -0300

    Adicionando minha foto.

---
+++ imagem/foto_perfil_files/u6iRKDVYXl1.js
@@ -0,0 +1,5 @@
+if (self.CavalryLogger) { CavalryLogger.start_js_script(document.currentScript); }/*FB_PKG_DELIM*/
+
+__d("usePolarisGetQuery",["usePolarisQueryStore"],(function(a,b,c,d,e,f,g){"use strict";function a(a,b,d){var e=c("usePolarisQueryStore")();return e.loadGetQuery([a,b],d)}g["default"]=a}),98);
+__d("usePolarisGetQueryResponse",["PolarisQueryBaseHooks"],(function(a,b,c,d,e,f,g){"use strict";function a(a){return d("PolarisQueryBaseHooks").usePolarisAnyQueryResponse(a,"GET")}g["default"]=a}),98);
+__d("usePolarisGetQuerySetup",["PolarisQueryBaseHooks"],(function(a,b,c,d,e,f,g){"use strict";function a(a,b){d("PolarisQueryBaseHooks").usePolarisAnyQuerySetup(a,b,"GET")}g["default"]=a}),98);
